47dde5e4c8f6a8609163293559c03bf42bbdabe0
Elendu Uche <eld.solution@gmail.com>
2018-12-19 08:25:44 +0100

    added authentication and secured the APIs

--- DatingApp.API/Dtos/UserForLoginDto.cs
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -2,10 +2,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Dtos
 {
-    public class UserForRegisterData
+    public class UserForLoginDto
     {
         [Required]
+        [StringLength(15, MinimumLength=4, ErrorMessage="Username length must be between 4 and 15 characters long")]
         public string Username { get; set; }
+
         [Required]
         [StringLength(8, MinimumLength=4, ErrorMessage="Password length must be between 4 and 8 characters long")]
         public string Password { get; set; }
